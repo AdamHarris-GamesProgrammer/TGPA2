376dbafb37273ec2df5b5952c803dcd38cf8cfe8
Adam Harris <adam_harris@hotmail.com>
2021-01-22 10:54:46 +0000

    0x02: Basic movment and sprites
    Added the SuperTiled2Unity library and created a small room for the player to move in.
    
    Added basic player prefab object with no sprite
    Player object can move around based on WASD
    Player movement can be found in the script PlayerMover this is in the TGP.Player namespace and is in the _Scripts > Player folder

---
+++ The Roach Collector/Assets/Sprites/Interiors/Dungeon Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Dungeon Tileset" tilewidth="16" tileheight="16" tilecount="480" columns="20">
+ <image source="tiles_dungeon_v1.1.png" width="320" height="384"/>
+</tileset>
---
+++ The Roach Collector/Assets/_Scripts/Player/PlayerMover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TGP.Player
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        [Header("Movement Settings")]
+        [Min(0f)][SerializeField] float _movementSpeed = 2.0f;
+
+        
+        void FixedUpdate()
+        {
+            //Gets the horizontal and vertical movement
+            float xMovement = Input.GetAxis("Horizontal");
+            float yMovement = Input.GetAxis("Vertical");
+
+            //Gets the current position
+            Vector2 currentPosition = transform.position;
+
+            //Adds the movement to the current position
+            currentPosition += new Vector2(xMovement, yMovement) * Time.deltaTime * _movementSpeed;
+
+            Debug.Log(string.Format("Movement ({0},{1})", xMovement, yMovement));
+
+            //Sets the new position
+            transform.position = currentPosition;
+        }
+    }
+}
+
