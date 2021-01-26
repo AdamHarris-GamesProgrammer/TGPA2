1541eb16789a67a7a68652d3b468434b70dc961b
Adam Harris <adam_harris@hotmail.com>
2021-01-26 13:21:02 +0000

    0x04: Removed Redundant Assets

--- The Roach Collector/Assets/_Scripts/Player/MouseLook.cs
+++ The Roach Collector/Assets/_Scripts/Player/MouseLook.cs
@@ -2,32 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
+namespace TGP.Player
 {
-    [Header("Mouse Settings")]
-    [SerializeField] float _mouseSensitivity = 100.0f;
-
-    float _xRotation = 0.0f;
-
-    private Transform _playerBody;
-
-    private void Awake()
+    public class MouseLook : MonoBehaviour
     {
-        _playerBody = transform.parent.GetComponent<Transform>();
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+        [Header("Mouse Settings")]
+        [SerializeField] float _mouseSensitivity = 100.0f;
 
-    void Update()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+        float _xRotation = 0.0f;
 
-        _playerBody.Rotate(Vector3.up * mouseX);
+        private Transform _playerBody;
 
-        _xRotation -= mouseY;
+        private void Awake()
+        {
+            _playerBody = transform.parent.GetComponent<Transform>();
+            Cursor.lockState = CursorLockMode.Locked;
+        }
 
-        _xRotation = Mathf.Clamp(_xRotation, -89.9f, 89.9f);
+        void Update()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
 
-        transform.localRotation = Quaternion.Euler(_xRotation, 0.0f, 0.0f);
+            _playerBody.Rotate(Vector3.up * mouseX);
+
+            _xRotation -= mouseY;
+
+            _xRotation = Mathf.Clamp(_xRotation, -89.9f, 89.9f);
+
+            transform.localRotation = Quaternion.Euler(_xRotation, 0.0f, 0.0f);
+        }
     }
 }
+
