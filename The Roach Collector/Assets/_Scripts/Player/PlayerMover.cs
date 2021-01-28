9476419ad8d88e4adb7e8a6107a5aef3219bead3
Adam Harris <adam_harris@hotmail.com>
2021-01-28 16:00:47 +0000

    0x05: Implemented UFPS animations

--- The Roach Collector/Assets/_Scripts/Player/PlayerMover.cs
+++ The Roach Collector/Assets/_Scripts/Player/PlayerMover.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TGP.Movement;
 
 namespace TGP.Player
 {
@@ -16,71 +17,30 @@ namespace TGP.Player
         [SerializeField] private LayerMask _groundMask;
 
 
+        Mover _mover;
+
         [Header("Crouch Settings")]
         [SerializeField] private float _verticalOffet = -0.4f;
         [Range(0.01f, 1.0f)][SerializeField] private float _crouchSpeedMultiplier = 0.5f;
 
-        //State Variables
-        private CharacterController _controller;
 
         private Animator _animator;
 
-        float _gravity = -9.81f;
-        bool _isGrounded;
-
-        Vector3 _velocity;
-
-        bool _isCrouched = false;
-
-
-        float _multiplier = 1.0f;
 
         private void Awake()
         {
             //Get the Character Controller
-            _controller = GetComponent<CharacterController>();
             _animator = GetComponent<Animator>();
+
+            _mover = GetComponent<Mover>();
         }
 
         void Update()
         {
-            
-            //Check if we are grounded
-            _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
-
-            //Set the velocity to -2 if we are grounded
-            if(_isGrounded && _velocity.y < 0)
-            {
-                _velocity.y = -2f;
-            }
-
-
-            //Crouching
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                Vector3 cameraPosition = Camera.main.transform.position;
-
-                if (_isCrouched)
-                {
-                    cameraPosition.y -= _verticalOffet;
-                    _multiplier = 1.0f;
-                }
-                else
-                {
-                    cameraPosition.y += _verticalOffet;
-                    _multiplier = _crouchSpeedMultiplier;
-                }
-
-                Camera.main.transform.position = cameraPosition;
-                _isCrouched = !_isCrouched;
-            }
-
-
-
             //Gets the horizontal and vertical movement
             Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-            if(input != Vector2.zero)
+            if (input != Vector2.zero)
             {
                 _animator.SetBool("Walk", true);
             }
@@ -92,20 +52,10 @@ namespace TGP.Player
             //Calculates the movement vector
             Vector3 move = transform.right * input.x + transform.forward * input.y;
 
-            //Moves us in the X and Z plane
-            _controller.Move(move * (_movementSpeed * _multiplier) * Time.deltaTime);
 
-            //Jump Controls
-            if(Input.GetButtonDown("Jump") && _isGrounded)
-            {
-                _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
-            } 
+            Vector3 position = transform.position + move;
 
-            //Adds gravity to our velocity calculation
-            _velocity.y += _gravity * Time.deltaTime;
-
-            //Moves us in the Y axis
-            _controller.Move(_velocity * Time.deltaTime);
+            _mover.MoveTo(position);
         }
     }
 }
