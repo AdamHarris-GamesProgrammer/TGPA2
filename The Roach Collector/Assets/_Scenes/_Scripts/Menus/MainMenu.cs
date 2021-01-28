b3e3b3e59055990b605ba5b28253152539668a23
Isaac Dunn <gandalfthebignose@gmail.com>
2021-01-28 15:47:59 +0000

    0x06 Setup scene manager for dev level
    Setup tutorial button to divert to dev level for testing purposes.

--- The Roach Collector/Assets/_Scenes/_Scripts/Menus/MainMenu.cs
+++ The Roach Collector/Assets/_Scenes/_Scripts/Menus/MainMenu.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
+        //go to level menu
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Tutorial()
     {
-        
+        SceneManager.LoadScene("_Dev");
+    }
+    public void Setting()
+    {
+        //do settings
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
