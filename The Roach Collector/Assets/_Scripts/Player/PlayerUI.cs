da9ed2c70dc8b3c07d01d73732cd01bb1d0f7f71
Isaac Dunn <gandalfthebignose@gmail.com>
2021-01-28 14:38:42 +0000

    0x03 Removed crosshair
    After change from 1st person to Top down the crosshair has now been removed.

--- The Roach Collector/Assets/_Scripts/Player/PlayerUI.cs
+++ The Roach Collector/Assets/_Scripts/Player/PlayerUI.cs
@@ -13,31 +13,22 @@ public class PlayerUI : MonoBehaviour
     public Text amnoTxt;
     public GameObject[] hearts;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Update_Lives_UI(lives);
-        Update_Amno_UI(clip, clipSize, amnoLeft);
-    }
-
+    //update number of lives whenever player is hurt or healed. lives is for the number of lives left
     public void Update_Lives_UI(int lives)
     {
+        //existing lives
         for (int i = 0; i < lives; i++)
         {
             hearts[i].SetActive(true);
         }
+        //lost lives
         for (int i = lives; i < hearts.Length; i++)
         {
             hearts[i].SetActive(false);
         }
     }
 
+    //update amno whenever player shoots, reloads or gains amno. clip is amno in clip. clipsize is for amno in each reload and anmoLeft is total amno
     public void Update_Amno_UI(int clip, int clipsize, int amnoLeft)
     {
         amnoTxt.text = clip + "/ " + (amnoLeft - clipsize);
