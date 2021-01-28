af4ad2b91ea9a898b410022e59477e0e2f52b1df
Adam Harris <adam_harris@hotmail.com>
2021-01-28 16:21:49 +0000

    0x06: Made a character prefab

--- The Roach Collector/Assets/_Scripts/Player/PlayerMover.cs
+++ The Roach Collector/Assets/_Scripts/Player/PlayerMover.cs
@@ -7,16 +7,6 @@ namespace TGP.Player
 {
     public class PlayerMover : MonoBehaviour
     {
-        [Header("Movement Settings")]
-        [Min(0f)] [SerializeField] float _jumpHeight = 2.45f;
-        [Min(0f)] [SerializeField] float _movementSpeed = 2.0f;
-
-        [Header("Ground Settings")]
-        [SerializeField] private Transform _groundCheck;
-        [SerializeField] private float _groundDistance = 0.4f;
-        [SerializeField] private LayerMask _groundMask;
-
-
         Mover _mover;
 
         [Header("Crouch Settings")]
@@ -24,14 +14,8 @@ namespace TGP.Player
         [Range(0.01f, 1.0f)][SerializeField] private float _crouchSpeedMultiplier = 0.5f;
 
 
-        private Animator _animator;
-
-
         private void Awake()
         {
-            //Get the Character Controller
-            _animator = GetComponent<Animator>();
-
             _mover = GetComponent<Mover>();
         }
 
@@ -40,18 +24,8 @@ namespace TGP.Player
             //Gets the horizontal and vertical movement
             Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-            if (input != Vector2.zero)
-            {
-                _animator.SetBool("Walk", true);
-            }
-            else
-            {
-                _animator.SetBool("Walk", false);
-            }
-
             //Calculates the movement vector
             Vector3 move = transform.right * input.x + transform.forward * input.y;
-
 
             Vector3 position = transform.position + move;
 
