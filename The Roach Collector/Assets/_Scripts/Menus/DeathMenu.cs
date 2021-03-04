a46d2caa4e5b823ad8854af8051a01214b52b5f4
bruncioh <57526069+bruncioh@users.noreply.github.com>
2021-03-04 14:41:18 +0000

    death screen sceen update

--- The Roach Collector/Assets/_Scripts/Menus/DeathMenu.cs
+++ The Roach Collector/Assets/_Scripts/Menus/DeathMenu.cs
@@ -5,7 +5,24 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class DeathMenu : MonoBehaviour
-{ 
+{
+    private bool isDead;
+    private Health _health;
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+    }
+    private void Update()
+    {
+        if (_health.IsDead())
+        {
+            SceneManager.LoadScene("DeathScreen");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _health.TakeDamage(10.0f);
+        }
+    }
     public void Respawn()
     {
         SceneManager.LoadScene("_Dev");
