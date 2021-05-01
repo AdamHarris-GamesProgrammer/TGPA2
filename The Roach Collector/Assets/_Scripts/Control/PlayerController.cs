f99d300af7f4b137b5ba5e0baa15e712f5f3e3e5
Adam Harris <adam_harris@hotmail.com>
2021-05-01 12:46:22 +0100

    Implemented Action slot controls to the game

--- The Roach Collector/Assets/_Scripts/Control/PlayerController.cs
+++ The Roach Collector/Assets/_Scripts/Control/PlayerController.cs
@@ -1,3 +1,4 @@
+using Harris.Inventories;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,16 +10,22 @@ namespace TGP.Control
     {
         bool _canDisableAlarm = false;
 
+
+
         public bool CanDisableAlarm { get { return _canDisableAlarm; } set { _canDisableAlarm = value; } }
         AlarmController _alarm;
 
         public AlarmController Alarm { set { _alarm = value; } }
 
+        ActionStore _actionSlots;
 
 
         private void Awake()
         {
+            _actionSlots = GetComponent<ActionStore>();
         }
+
+ 
 
         private void Update()
         {
@@ -29,13 +36,38 @@ namespace TGP.Control
                     _alarm.DisableAlarm();
                 }
             }
+
+            InteractWithActionBar();
+
         }
 
-        private void FixedUpdate()
+        private void InteractWithActionBar()
         {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                _actionSlots.Use(0, this.gameObject);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                _actionSlots.Use(1, this.gameObject);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                _actionSlots.Use(2, this.gameObject);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                _actionSlots.Use(3, this.gameObject);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                _actionSlots.Use(4, this.gameObject);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                _actionSlots.Use(5, this.gameObject);
+            }
         }
-
-
     }
 }
 
