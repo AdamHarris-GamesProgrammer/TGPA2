cef9e325fd5b58a38312cdfd610b2e1b714fb6cb
Adam Harris <32218978+AdamHarris-GamesProgrammer@users.noreply.github.com>
2021-05-06 12:23:03 +0100

    Looks at point

--- The Roach Collector/Assets/_Scripts/AI/AICheckPlayerState.cs
+++ The Roach Collector/Assets/_Scripts/AI/AICheckPlayerState.cs
@@ -8,6 +8,8 @@ public class AICheckPlayerState : AIState
     LastKnownLocation _lastKnownLocation;
     NavMeshAgent _navAgent;
 
+    bool _arrivedAtPoint = false;
+
     public AICheckPlayerState(AIAgent agent)
     {
         _navAgent = agent.GetComponent<NavMeshAgent>();
@@ -16,6 +18,8 @@ public class AICheckPlayerState : AIState
 
     public void Enter(AIAgent agent)
     {
+        _arrivedAtPoint = false;
+
         if(_lastKnownLocation == null)
         {
             _lastKnownLocation = GameObject.FindObjectOfType<LastKnownLocation>();
@@ -24,7 +28,7 @@ public class AICheckPlayerState : AIState
         bool successful = false;
         Vector3 finalDestination = Vector3.zero;
 
-        Debug.Log("check player state");
+        //Debug.Log("check player state");
 
         do
         {
@@ -41,19 +45,19 @@ public class AICheckPlayerState : AIState
             point.x = x;
             point.y = y;
 
-            Debug.Log("Point: " + point);
+            //Debug.Log("Point: " + point);
 
             Vector3 destination = new Vector3(point.x * _lastKnownLocation.RadiusAroundPlayer / 2, agent.GetPlayer().position.y, point.y * _lastKnownLocation.RadiusAroundPlayer / 2);
 
-            Debug.Log("Destination: " + destination);
+            //Debug.Log("Destination: " + destination);
 
             finalDestination = agent.GetPlayer().position + destination;
 
-            Debug.Log("Final Destination: " + finalDestination);
+            //Debug.Log("Final Destination: " + finalDestination);
 
             successful = Physics.Raycast(finalDestination, _lastKnownLocation.transform.position - finalDestination);
 
-            Debug.Log("Successful: " + successful);
+            //Debug.Log("Successful: " + successful);
 
         } while (!successful);
 
@@ -67,15 +71,26 @@ public class AICheckPlayerState : AIState
 
     public void Update(AIAgent agent)
     {
-        if(_navAgent.pathStatus == NavMeshPathStatus.PathComplete)
+        if(!_arrivedAtPoint && _navAgent.pathStatus == NavMeshPathStatus.PathComplete)
         {
+            _arrivedAtPoint = true;
+
             Vector3 direction = _lastKnownLocation.transform.position - agent.transform.position;
 
             Quaternion look = Quaternion.Slerp(agent.transform.rotation, Quaternion.LookRotation(direction, Vector3.up), Time.deltaTime);
 
             agent.transform.rotation = look;
         }
+
+        //TODO: Wait for timer saying they should go over to the player
+        //Or if there are sufficient allies in the area 
+
+        //Then select a single AI to go over and see if the player is there
+            //If the agent is there and the player is then actrivate attack mode
+            //if the agent is there and the player has gone then activate search mode.
+                
     }
+
 
     public AiStateId GetID()
     {
