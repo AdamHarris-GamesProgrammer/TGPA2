ada6b6c08fad20c22d4a9880f6574c1e9baf218d
Isaac Dunn <57407700+IsaacdmDunn@users.noreply.github.com>
2021-05-13 12:25:46 +0100

    Added Logo and End level UI Bluescript
    added logo to the main menu
    also added script blueprint to handle with end level UI

---
+++ The Roach Collector/Assets/_Scripts/Level/LevelEndPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEndPoint : MonoBehaviour
+{
+    void OnCollisionEnter(Collision levelEndPoint)
+    {
+        //TODO make level end UI transition
+    }
+}
---
+++ The Roach Collector/Assets/_Scripts/Menus/EndLevelUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndLevelUI : MonoBehaviour
+{
+    public string LevelName;
+    public Text LevelNameTXT;
+    public Text Score;
+    public Text Time;
+
+    public void NextLevel()
+    {
+
+    }
+
+    public void GoToHideout() 
+    {
+
+    }
+
+    public void MainMenu() 
+    {
+
+    }
+
+    public void Retry()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void GetBestScore()
+    {
+
+    }
+
+    void GetBestTime()
+    {
+
+    }
+
+    void SetBestScore()
+    {
+
+    }
+
+    void SetBestTime()
+    {
+
+    }
+
+    void SetLevelName()
+    {
+
+    }
+}
