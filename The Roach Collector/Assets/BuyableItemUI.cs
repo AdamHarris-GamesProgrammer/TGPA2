0836f522c7b999b31ff6dc423ed606c3e9c65746
Adam Harris <adam_harris@hotmail.com>
2021-05-14 11:52:24 +0100

    Created prefab for buyable item

---
+++ The Roach Collector/Assets/BuyableItemUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyableItemUI : MonoBehaviour
+{
+    [SerializeField] Image _itemImage;
+    [SerializeField] Text _itemName;
+    [SerializeField] Text _itemPrice;
+    [SerializeField] Text _itemQuantity;
+
+    public void Setup(Sprite icon, string name, float price, int quantity)
+    {
+        _itemImage.sprite = icon;
+        _itemName.text = name;
+        _itemPrice.text = "Price: " + price.ToString("#0.00");
+        _itemQuantity.text = "x" + quantity;
+    }
+
+}
