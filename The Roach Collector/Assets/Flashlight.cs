e2badaacab6440fa170837cb3d622b150013509c
Adam Harris <32218978+AdamHarris-GamesProgrammer@users.noreply.github.com>
2021-05-20 10:36:05 +0100

    Added a flashlight to the player

---
+++ The Roach Collector/Assets/Flashlight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    Light _flashlight;
+
+    void Awake() {
+        _flashlight = GetComponent<Light>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T)){
+            _flashlight.enabled = !_flashlight.enabled;
+        }    
+
+        _flashlight.transform.forward = Camera.main.transform.forward;
+    }
+}
