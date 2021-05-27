430cf1f7e8ad91eeab0125e3c2b9f40bf34ffcbc
Isaac Dunn <57407700+IsaacdmDunn@users.noreply.github.com>
2021-05-27 15:55:07 +0100

    Minor UI changes to level selector

--- The Roach Collector/Assets/_Scripts/Menus/LevelSelector.cs
+++ The Roach Collector/Assets/_Scripts/Menus/LevelSelector.cs
@@ -5,60 +5,40 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Harris.Saving;
 
-public class LevelSelector : MonoBehaviour, ISaveable
+public class LevelSelector : MonoBehaviour
 {
     LevelDataStruct levelData;
 
-    public int levelID = 0;
     //TODO: Change this as level grow
     public int levelCount = 2;
+    public int levelID = 0;
     public string[] levelName;
-    public float[] levelTimeRecord;
-    public int[] levelScore;
-    public int[] levelRank;
-    public int[] levelRoaches;
+    //public float[] levelTimeRecord;
+    //public int[] levelScore;
+    //public int[] levelRank;
+    //public int[] levelRoaches;
     public Sprite[] levelImage;
     public Image levelMenuImage;
 
     public GameObject LevelMenu;
 
     public Text LevelNameTXT;
-    public Text LevelScoreTXT;
-    public Text LevelTimeTXT;
+    //public Text LevelScoreTXT;
+    //public Text LevelTimeTXT;
 
-    [SerializeField] private Sprite[] rankSprites = new Sprite[6];
-    [SerializeField] private Image rankImage;
+    //[SerializeField] private Sprite[] rankSprites = new Sprite[6];
+    //[SerializeField] private Image rankImage;
 
-    public Image RoachToken1;
-    public Image RoachToken2;
-    public Image RoachToken3;
-    public Sprite RoachTokenSprite;
-    public Sprite RoachEmptyTokenSprite;
+    //public Image RoachToken1;
+    //public Image RoachToken2;
+    //public Image RoachToken3;
+    //public Sprite RoachTokenSprite;
+    //public Sprite RoachEmptyTokenSprite;
 
-    private void Start()
+    private void Awake()
     {
         //Loads level data
-        //FindObjectOfType<SavingWrapper>().Load();
-        
-    }
-
-
-    private void OnEnable()
-    {
-        levelRank = new int[2];
-        levelRoaches = new int[2];
-
-        
-    }
-
-    private void OnLevelLoad()
-    {
-        
         UpdateUI();
-    }
-
-    private void Update()
-    {
     }
 
     //exits menu
@@ -73,93 +53,23 @@ public class LevelSelector : MonoBehaviour, ISaveable
         LevelNameTXT.text = levelName;
     }
 
-    //sets record time of level in level menu
-    void SetLevelTime(float levelTime)
-    {
-        LevelTimeTXT.text = levelTime.ToString();
-    }
-
-    //sets record score of level in level menu
-    void SetLevelScore(int score)
-    {
-        LevelScoreTXT.text = score.ToString();
-    }
-
     //sets image of level in level menu
     void SetLevelImage(Sprite levelSprite)
     {
         levelMenuImage.sprite = levelSprite;
     }
 
-    void SetRank(int rank)
-    {
-        switch (rank)
-        {
-            //f
-            case 0:
-                rankImage.sprite = rankSprites[5];
-                break;
-            //d
-            case 1:
-                rankImage.sprite = rankSprites[5];
-                break;
-            //c
-            case 2:
-                rankImage.sprite = rankSprites[5];
-                break;
-            //b
-            case 3:
-                rankImage.sprite = rankSprites[5];
-                break;
-            //a
-            case 4:
-                rankImage.sprite = rankSprites[5];
-                break;
-            //s
-            case 5:
-                rankImage.sprite = rankSprites[5];
-                break;
-        }
-    }
+    
 
-    void SetRoaches(int roachCount)
-    {
-        switch (roachCount)
-        {
-            case 0:
-                RoachToken1.sprite = RoachEmptyTokenSprite;
-                RoachToken2.sprite = RoachEmptyTokenSprite;
-                RoachToken3.sprite = RoachEmptyTokenSprite;
-                break;
-            case 1:
-                RoachToken1.sprite = RoachTokenSprite;
-                RoachToken2.sprite = RoachEmptyTokenSprite;
-                RoachToken3.sprite = RoachEmptyTokenSprite;
-                break;
-            case 2:
-                RoachToken1.sprite = RoachTokenSprite;
-                RoachToken2.sprite = RoachTokenSprite;
-                RoachToken3.sprite = RoachEmptyTokenSprite;
-                break;
-            case 3:
-                RoachToken1.sprite = RoachTokenSprite;
-                RoachToken2.sprite = RoachTokenSprite;
-                RoachToken3.sprite = RoachTokenSprite;
-                break;
-            default:
-                break;
-
-        }
-    }
-
+    //updates UI for the level selector
     void UpdateUI()
     {
         SetLevelName(levelName[levelID]);
-        SetLevelTime(levelTimeRecord[levelID]);
-        SetLevelScore(levelScore[levelID]);
         SetLevelImage(levelImage[levelID]);
-        SetRank(levelRank[levelID]);
-        SetRoaches(levelRoaches[levelID]);
+        //SetLevelTime(levelTimeRecord[levelID]);
+        //SetLevelScore(levelScore[levelID]);
+        //SetRank(levelRank[levelID]);
+        //SetRoaches(levelRoaches[levelID]);
     }
 
     //change to next level
@@ -203,38 +113,111 @@ public class LevelSelector : MonoBehaviour, ISaveable
     }
 
     //load level data from 
-    public void Load(object state)
-    {
-        Debug.Log("Bee");
-        LevelDataStruct[] data = new LevelDataStruct[2];
-        data = (LevelDataStruct[])state;
-        Debug.Log("Boop: " + data.Length);
-        for(int i = 0; i < data.Length; i++)
-        {
-            levelScore[i] = data[i].highscore;
-            levelTimeRecord[i] = data[i].bestTime;
-            Debug.Log(data[i].bestTime);
-            levelRank[i] = data[i].highestRank;
-            levelRoaches[i] = data[i].roachesCollected;
-        }
+    //public void Load(object state)
+    //{
+    //    Debug.Log("Bee");
+    //    LevelDataStruct[] data = new LevelDataStruct[2];
+    //    data = (LevelDataStruct[])state;
+    //    Debug.Log("Boop: " + data.Length);
+    //    for(int i = 0; i < data.Length; i++)
+    //    {
+    //        levelScore[i] = data[i].highscore;
+    //        levelTimeRecord[i] = data[i].bestTime;
+    //        Debug.Log(data[i].bestTime);
+    //        levelRank[i] = data[i].highestRank;
+    //        levelRoaches[i] = data[i].roachesCollected;
+    //    }
 
 
-    }
+    //}
 
-    LevelDataStruct[] data;
+    //LevelDataStruct[] data;
 
-    public object Save()
-    {
-        data = new LevelDataStruct[2];
+    //public object Save()
+    //{
+    //    data = new LevelDataStruct[2];
 
-        for(int i = 0; i < 2; i++)
-        {
-            data[i].bestTime = levelTimeRecord[i];
-            data[i].highscore = levelScore[i];
-            data[i].highestRank = levelRank[i];
-            data[i].roachesCollected = levelRoaches[i];
-        }
+    //    for(int i = 0; i < 2; i++)
+    //    {
+    //        data[i].bestTime = levelTimeRecord[i];
+    //        data[i].highscore = levelScore[i];
+    //        data[i].highestRank = levelRank[i];
+    //        data[i].roachesCollected = levelRoaches[i];
+    //    }
 
-        return data;
-    }
+    //    return data;
+    //}
+
+    //void SetRank(int rank)
+    //{
+    //    switch (rank)
+    //    {
+    //        //f
+    //        case 0:
+    //            rankImage.sprite = rankSprites[5];
+    //            break;
+    //        //d
+    //        case 1:
+    //            rankImage.sprite = rankSprites[5];
+    //            break;
+    //        //c
+    //        case 2:
+    //            rankImage.sprite = rankSprites[5];
+    //            break;
+    //        //b
+    //        case 3:
+    //            rankImage.sprite = rankSprites[5];
+    //            break;
+    //        //a
+    //        case 4:
+    //            rankImage.sprite = rankSprites[5];
+    //            break;
+    //        //s
+    //        case 5:
+    //            rankImage.sprite = rankSprites[5];
+    //            break;
+    //    }
+    //}
+
+    //void SetRoaches(int roachCount)
+    //{
+    //    switch (roachCount)
+    //    {
+    //        case 0:
+    //            RoachToken1.sprite = RoachEmptyTokenSprite;
+    //            RoachToken2.sprite = RoachEmptyTokenSprite;
+    //            RoachToken3.sprite = RoachEmptyTokenSprite;
+    //            break;
+    //        case 1:
+    //            RoachToken1.sprite = RoachTokenSprite;
+    //            RoachToken2.sprite = RoachEmptyTokenSprite;
+    //            RoachToken3.sprite = RoachEmptyTokenSprite;
+    //            break;
+    //        case 2:
+    //            RoachToken1.sprite = RoachTokenSprite;
+    //            RoachToken2.sprite = RoachTokenSprite;
+    //            RoachToken3.sprite = RoachEmptyTokenSprite;
+    //            break;
+    //        case 3:
+    //            RoachToken1.sprite = RoachTokenSprite;
+    //            RoachToken2.sprite = RoachTokenSprite;
+    //            RoachToken3.sprite = RoachTokenSprite;
+    //            break;
+    //        default:
+    //            break;
+
+    //    }
+    //}
+
+    ////sets record time of level in level menu
+    //void SetLevelTime(float levelTime)
+    //{
+    //    LevelTimeTXT.text = levelTime.ToString();
+    //}
+
+    ////sets record score of level in level menu
+    //void SetLevelScore(int score)
+    //{
+    //    LevelScoreTXT.text = score.ToString();
+    //}
 }
