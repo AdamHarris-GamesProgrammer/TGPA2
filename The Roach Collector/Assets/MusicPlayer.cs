eaeb93a20af0c4c2fdf6967a5a0ef6227335ba28
Isaac Dunn <57407700+IsaacdmDunn@users.noreply.github.com>
2021-06-03 14:23:22 +0100

    Added MusicPlayer class with music tracks

---
+++ The Roach Collector/Assets/MusicPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource[] _track;
+    [SerializeField] private int _currentTrack;
+
+    public void Start()
+    {
+        _track[_currentTrack].Play();
+    }
+
+    //plays chosen track
+    public void Play(int trackID)
+    {
+        _track[trackID].Play();
+    }
+
+    //pauses current track
+    public void PauseMusic()
+    {
+        _track[_currentTrack].Pause();
+    }
+
+    //unpauses current track
+    public void UnpauseMusic()
+    {
+        _track[_currentTrack].Pause();
+    }
+
+    //stops track
+    public void StopMusic()
+    {
+        _track[_currentTrack].Stop();
+    }
+
+    //changes pitch of current track
+    public void ChangePitch(float pitch)
+    {
+        _track[_currentTrack].pitch = pitch;
+    }
+
+    //change volume of current track
+    public void ChangeVolume(float volume)
+    {
+        _track[_currentTrack].volume = volume;
+    }
+
+}
